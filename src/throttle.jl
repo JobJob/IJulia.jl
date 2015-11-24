35340c65379ee0e2f231937803d3d4280d3f6949
JobJob <jobba1@hotmail.com>
2015-11-25 01:40:48 +0200

    Added comments to `throttle`

--- src/throttle.jl
+++ src/throttle.jl
@@ -5,32 +5,36 @@ Normally, the throttled function will run as much as it can, without ever going 
 but if you’d like to disable the execution on the leading edge, pass leading=false.
 To disable execution on the trailing edge, ditto.
 """
-
 throttle(func, wait; leading=true, trailing=true) = begin
-    timer = Timer((t)->nothing,0.0) #dummy timer that we close immediately
+    timer = Timer((t)->nothing,0.0)
+    #dummy `Timer` that we close immediately to avoid having
+    #to check for `timer` being undefined below
     close(timer)
     previous = 0.0
     return throttled_func(args...) = begin
         later(t) = begin
+            #later is the function that is scheduled when the task needs to be run later
             previous = leading == false ? 0.0 : time()
             close(timer)
-            # println(orig_STDOUT, get_log_ts(), "here's one we prepared earlier")
             result = func(args...)
         end
         now = time()
         if previous == 0.0 && leading == false
             previous = now
-            # println(orig_STDOUT, get_log_ts(), "Your time starts... now")
         end
         remaining = wait - (now - previous)
         if (remaining <= 0.0)
+            #the function is ready to be called now
             if (isopen(timer))
+                #we're calling func now so close the timer that was ready to call
+                #it to avoid calling twice
                 close(timer)
             end
             previous = now
-            # println(orig_STDOUT, get_log_ts(), "good timing, remaining: $remaining")
             func(args...)
         elseif (!isopen(timer) && trailing != false)
+            #if no timer has been set up (and trailing is enabled) then set one up to call the
+            #function once the `wait` is over.
             timer = Timer(later, remaining)
         end
         return timer
